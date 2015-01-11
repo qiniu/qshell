266e47473976485b2e2c86fb29775e28c0e417b3
Jemy <jemygraw@gmail.com>
2015-01-11 09:39:54 +0800

    Add resumable upload function.

--- src/cli/fop.go
+++ src/cli/fop.go
@@ -13,7 +13,7 @@ func Prefop(cmd string, params ...string) {
 		fopRet := qshell.FopRet{}
 		err := rsfopS.Prefop(persistentId, &fopRet)
 		if err != nil {
-			log.Error(fmt.Sprintf("Can not get fop status for `%s'", persistentId), err)
+			log.Error(fmt.Sprintf("Can not get fop status for `%s',", persistentId), err)
 		} else {
 			fmt.Println(fopRet.String())
 		}
--- src/cli/putfile.go
+++ src/cli/putfile.go
@@ -3,9 +3,12 @@ package cli
 import (
 	"fmt"
 	"github.com/qiniu/api/auth/digest"
-	"github.com/qiniu/api/io"
+	fio "github.com/qiniu/api/io"
+	rio "github.com/qiniu/api/resumable/io"
 	"github.com/qiniu/api/rs"
 	"github.com/qiniu/log"
+	"os"
+	"sync/atomic"
 )
 
 func FormPut(cmd string, params ...string) {
@@ -21,13 +24,13 @@ func FormPut(cmd string, params ...string) {
 		mac := digest.Mac{accountS.AccessKey, []byte(accountS.SecretKey)}
 		policy := rs.PutPolicy{}
 		policy.Scope = bucket
-		putExtra := io.PutExtra{}
+		putExtra := fio.PutExtra{}
 		if mimeType != "" {
 			putExtra.MimeType = mimeType
 		}
 		uptoken := policy.Token(&mac)
-		putRet := io.PutRet{}
-		err := io.PutFile(nil, &putRet, uptoken, key, localFile, &putExtra)
+		putRet := fio.PutRet{}
+		err := fio.PutFile(nil, &putRet, uptoken, key, localFile, &putExtra)
 		if err != nil {
 			log.Error("Put file error", err)
 		} else {
@@ -39,5 +42,62 @@ func FormPut(cmd string, params ...string) {
 }
 
 func ResumablePut(cmd string, params ...string) {
+	if len(params) == 3 || len(params) == 4 {
+		bucket := params[0]
+		key := params[1]
+		localFile := params[2]
+		mimeType := ""
+		if len(params) == 4 {
+			mimeType = params[3]
+		}
+		fi, err := os.Stat(localFile)
+		if err != nil {
+			log.Error("Stat local file error,", err)
+			return
+		}
+		fileSize := fi.Size()
+		accountS.Get()
+		mac := digest.Mac{accountS.AccessKey, []byte(accountS.SecretKey)}
+		policy := rs.PutPolicy{}
+		policy.Scope = bucket
+		putExtra := rio.PutExtra{}
+		if mimeType != "" {
+			putExtra.MimeType = mimeType
+		}
+		progressHandler := ProgressHandler{
+			fileSize,
+			0,
+		}
+		putExtra.Notify = progressHandler.Notify
+		uptoken := policy.Token(&mac)
+		putRet := rio.PutRet{}
+		err = rio.PutFile(nil, &putRet, uptoken, key, localFile, &putExtra)
+		if err != nil {
+			log.Error("\r\nPut file error", err)
+		} else {
+			fmt.Println("\r\nPut file", localFile, "=>", bucket, ":", putRet.Key, "(", putRet.Hash, ")", "success!")
+		}
+	} else {
+		Help(cmd)
+	}
+}
 
+type ProgressHandler struct {
+	FileSize int64
+	Offset   int64
+}
+
+func (this *ProgressHandler) Percent() float32 {
+	return float32(this.Offset) / float32(this.FileSize) * 100
+}
+
+func (this *ProgressHandler) Notify(blkIdx int, blkSize int, ret *rio.BlkputRet) {
+	offset := ret.Offset
+	atomic.AddInt64(&this.Offset, int64(offset))
+	percent := this.Percent()
+	output := fmt.Sprintf("Uploading %.2f%%", percent)
+	for i := 0; i < len(output); i++ {
+		fmt.Print("\b")
+	}
+	fmt.Print(output)
 }
