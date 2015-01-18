4d9154c2f4438b076fabf76b6f2b54f220aae761
Jemy <jemygraw@gmail.com>
2015-01-18 17:20:22 +0800

    Add more output info for the function profess.

--- src/qshell/rs_fop.go
+++ src/qshell/rs_fop.go
@@ -11,14 +11,28 @@ type RSFop struct {
 }
 
 type FopRet struct {
-	Id    string `json:"id"`
-	Code  int    `json:"code"`
-	Desc  string `json:"desc"`
-	Items []FopResult
+	Id       string `json:"id"`
+	Code     int    `json:"code"`
+	Desc     string `json:"desc"`
+	InputKey string `json:"inputKey,omitempty"`
+	Pipeline string `json:"pipeline,omitempty"`
+	Reqid    string `json:"reqid,omitempty"`
+	Items    []FopResult
 }
 
 func (this *FopRet) String() string {
-	strData := fmt.Sprintf("Id:\t%s\r\nCode:\t%d\r\nDesc:\t%s\r\n\r\n", this.Id, this.Code, this.Desc)
+	strData := fmt.Sprintf("Id: %s\r\nCode: %d\r\nDesc: %s\r\n", this.Id, this.Code, this.Desc)
+	if this.InputKey != "" {
+		strData += fmt.Sprintln(fmt.Sprintf("InputKey: %s", this.InputKey))
+	}
+	if this.Pipeline != "" {
+		strData += fmt.Sprintln(fmt.Sprintf("Pipeline: %s", this.Pipeline))
+	}
+	if this.Reqid != "" {
+		strData += fmt.Sprintln(fmt.Sprintf("Reqid: %s", this.Reqid))
+	}
+
+	strData = fmt.Sprintln(strData)
 	for _, item := range this.Items {
 		strData += fmt.Sprintf("\tCmd:\t%s\r\n\tCode:\t%d\r\n\tDesc:\t%s\r\n", item.Cmd, item.Code, item.Desc)
 		if item.Error != "" {
