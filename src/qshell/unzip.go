e6a888b147fc4cb80b10c34dee8907af24966ab3
Jemy <jemygraw@gmail.com>
2015-03-27 16:42:45 +0800

    Add detailed error info for unzip.

--- src/qshell/unzip.go
+++ src/qshell/unzip.go
@@ -2,48 +2,65 @@ package qshell
 
 import (
 	"archive/zip"
+	"errors"
+	"fmt"
+	"github.com/qiniu/iconv"
 	"io"
 	"os"
 	"path/filepath"
+	"unicode/utf8"
 )
 
 func Unzip(zipFilePath string, unzipPath string) (err error) {
 	zipReader, zipErr := zip.OpenReader(zipFilePath)
 	if zipErr != nil {
-		err = zipErr
+		err = errors.New(fmt.Sprintf("Open zip file error, %s", zipErr))
 		return
 	}
 	defer zipReader.Close()
 
 	zipFiles := zipReader.File
+	cd, cErr := iconv.Open("utf-8", "gbk")
+	if cErr != nil {
+		err = errors.New(fmt.Sprintf("Create charset converter error, %s", cErr))
+		return
+	}
+	defer cd.Close()
 	for _, zipFile := range zipFiles {
 		fileInfo := zipFile.FileHeader.FileInfo()
 		fileName := zipFile.FileHeader.Name
+
+		//check charset utf8 or gbk
+		if !utf8.Valid([]byte(fileName)) {
+			fileName = cd.ConvString(fileName)
+		}
+
 		fullPath := filepath.Join(unzipPath, fileName)
+
 		if fileInfo.IsDir() {
 			mErr := os.MkdirAll(fullPath, 0775)
 			if mErr != nil {
-				err = mErr
+				err = errors.New(fmt.Sprintf("Mkdir error, %s", mErr))
 				continue
 			}
 		} else {
 			localFp, openErr := os.OpenFile(fullPath, os.O_CREATE|os.O_WRONLY, 0666)
 			if openErr != nil {
-				err = openErr
+				err = errors.New(fmt.Sprintf("Open local file error, %s", openErr))
 				continue
 			}
 			defer localFp.Close()
 
 			zipFp, openErr := zipFile.Open()
 			if openErr != nil {
-				err = openErr
+				err = errors.New(fmt.Sprintf("Read zip content error, %s", openErr))
 				continue
 			}
 			defer zipFp.Close()
 
 			_, wErr := io.Copy(localFp, zipFp)
 			if wErr != nil {
-				err = wErr
+				err = errors.New(fmt.Sprintf("Save zip content error, %s", wErr))
 				continue
 			}
 		}
