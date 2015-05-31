ac2f484c3ac210decac9f351bea5e9415cf8c30b
jemygraw <jemygraw@gmail.com>
2015-05-31 16:08:49 +0800

    Add new gox build script.

--- src/gox_build.sh
+++ src/gox_build.sh
@@ -1,3 +1,8 @@
 export GOPATH=$GOPATH:/Users/jemy/QiniuCloud/Projects/qshell
-gox -os="windows"
-gox -os="darwin"
+gox -os="darwin" -arch="386"  
+gox -os="darwin" -arch="amd64"  
+gox -os="windows" -arch="386" 
+gox -os="windows" -arch="amd64"  
+gox -os="linux" -arch="386"  
+gox -os="linux" -arch="amd64" 
+mv src_* ../bin/
