37e07195a384748da51435973706469ed18a6fe8
jemygraw <jemygraw@gmail.com>
2015-05-31 17:53:15 +0800

    Update gox build script

--- src/gox_build.sh
+++ src/gox_build.sh
@@ -3,6 +3,7 @@ gox -os="darwin" -arch="386"
 gox -os="darwin" -arch="amd64"  
 gox -os="windows" -arch="386" 
 gox -os="windows" -arch="amd64"  
-gox -os="linux" -arch="386"  
-gox -os="linux" -arch="amd64" 
-mv src_* ../bin/
+mv src_darwin_386 ../bin/qshell_darwin_386
+mv src_darwin_amd64 ../bin/qshell_darwin_amd64
+mv src_windows_386.exe ../bin/qshell_windows_386.exe
+mv src_windows_amd64.exe ../bin/qshell_windows_amd64.exe
