d5bf0047896f000883a91159391285905825ad83
jemygraw <jemygraw@gmail.com>
2015-12-04 11:19:20 +0800

    add v1.6.4 download link

--- src/gox_build.sh
+++ src/gox_build.sh
@@ -1,5 +1,4 @@
 export GOPATH=$GOPATH:/Users/jemy/QiniuCloud/Projects/qshell
-gox -os="darwin" -arch="386"  -output="../bin/qshell_darwin_386"
 gox -os="darwin" -arch="amd64" -output="../bin/qshell_darwin_amd64" 
 gox -os="windows" -arch="386" -output="../bin/qshell_windows_386"
 gox -os="windows" -arch="amd64" -output="../bin/qshell_windows_amd64"
