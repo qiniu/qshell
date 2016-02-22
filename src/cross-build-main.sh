e44e95e30a52a75e1ff117ad73cdae7f11b3cc50
jemygraw <jemygraw@gmail.com>
2016-02-22 15:32:35 +0800

    discard gox cross build, after go 1.5, use the system standard cross platform build

---
+++ src/cross-build-main.sh
@@ -0,0 +1,8 @@
+export GOPATH=$GOPATH:/Users/jemy/QiniuCloud/Projects/qshell
+GOOS=linux   GOARCH=386    go build -o ../bin/qshell_linux_386     main.go
+GOOS=linux   GOARCH=amd64  go build -o ../bin/qshell_linux_amd64   main.go
+GOOS=linux   GOARCH=arm    go build -o ../bin/qshell_linux_arm     main.go
+GOOS=windows GOARCH=386    go build -o ../bin/qshell_windows_386.exe   main.go
+GOOS=windows GOARCH=amd64  go build -o ../bin/qshell_windows_amd64.exe main.go
+GOOS=darwin  GOARCH=386    go build -o ../bin/qshell_darwin_386    main.go
+GOOS=darwin  GOARCH=amd64  go build -o ../bin/qshell_darwin_amd64  main.go
