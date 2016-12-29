59fe61e1e314eeab4d0c61cb6e1fa3d459e052cc
jemygraw <jemygraw@gmail.com>
2016-12-29 17:10:45 +0800

    remove cdnwho and regional cmd, useless

--- src/cli/cdn.go
+++ src/cli/cdn.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"qiniu/api.v6/auth/digest"
 	"qiniu/api.v6/rs"
-	"qiniu/log"
 	"qshell"
 	"strings"
 )
@@ -15,63 +14,6 @@ const (
 	BATCH_CDN_REFRESH_ALLOW_MAX  = 100
 	BATCH_CDN_PREFETCH_ALLOW_MAX = 100
 )
-
-func GetCdnSupplierOfIp(cmd string, params ...string) {
-	if len(params) == 1 {
-		ip := params[0]
-		gErr := accountS.Get()
-		if gErr != nil {
-			fmt.Println(gErr)
-			return
-		}
-		mac := digest.Mac{
-			accountS.AccessKey,
-			[]byte(accountS.SecretKey),
-		}
-		rsClient := rs.NewMac(&mac)
-		cdnInfo, err := qshell.GetCdnSupplierOfIp(&rsClient, ip)
-		if err != nil {
-			log.Error("Get cdn supplier of ip error,", err)
-		} else {
-			//for _, cname := range cdnInfo.LineCname {
-			//	fmt.Println(cname)
-			//}
-			fmt.Println(cdnInfo.CdnInfo)
-			//fmt.Println(cdnInfo.IpAddress)
-		}
-	} else {
-		CmdHelp(cmd)
-	}
-}
-
-func GetCdnRegionalIps(cmd string, params ...string) {
-	if len(params) == 3 {
-		cname := params[0]
-		isp := params[1]
-		province := params[2]
-
-		gErr := accountS.Get()
-		if gErr != nil {
-			fmt.Println(gErr)
-			return
-		}
-		mac := digest.Mac{
-			accountS.AccessKey,
-			[]byte(accountS.SecretKey),
-		}
-		rsClient := rs.NewMac(&mac)
-		ips, err := qshell.GetCdnRegionalIps(&rsClient, cname, isp, province)
-		if err != nil {
-			log.Error("Get regional ips of cname error,", err)
-		} else {
-			for _, ip := range ips {
-				fmt.Println(ip)
-			}
-		}
-	} else {
-		CmdHelp(cmd)
-	}
-}
 
 func CdnRefresh(cmd string, params ...string) {
 	if len(params) == 1 {
