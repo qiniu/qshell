507ea4f522c791a1d8d39ea386f2576dda94a81e
蔡加强 <tonycai653@yahoo.com>
2018-11-29 14:06:18 +0800

    debug

--- iqshell/list_bucket.go
+++ iqshell/list_bucket.go
@@ -112,7 +112,11 @@ func (m *BucketManager) ListBucket2(bucket, prefix, marker, listResultFile, deli
 
 		if entries == nil && lErr == nil {
 			// no data
-			break
+			if lastMarker == "" {
+				break
+			} else {
+				continue
+			}
 		}
 		if lErr != nil {
 			retErr = lErr
