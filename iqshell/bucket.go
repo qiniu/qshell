f6586d1074648bb4b184035b9089d24dcc84ac5b
yangsen <yang_sen_ysys@163.com>
2021-06-24 15:12:05 +0800

    optimize get cmd logic

--- iqshell/bucket.go
+++ iqshell/bucket.go
@@ -203,10 +203,10 @@ func (m *BucketManager) Get(bucket, key string, destFile string) (err error) {
 	for {
 		f, err := os.OpenFile(destFile, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
 		if err == nil {
+			defer f.Close()
 			if _, err := io.Copy(f, resp.Body); err != nil {
 				fmt.Fprintf(os.Stderr, "Qget: err: %s\n", err)
 			}
-			f.Close()
 			break
 		} else if os.IsNotExist(err) {
 			destDir := filepath.Dir(destFile)
