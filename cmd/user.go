c1f8ce28b6b5e66e8794c3def89ef2cd6d601c8c
yangsen <yang_sen_ysys@163.com>
2021-12-06 17:00:36 +0800

    user add cmd: current & add

--- cmd/user.go
+++ cmd/user.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/qiniu/qshell/v2/iqshell/common/account"
 	"os"
+
+	"github.com/qiniu/qshell/v2/iqshell/common/account"
+	"github.com/qiniu/qshell/v2/iqshell/common/alert"
+	"github.com/qiniu/qshell/v2/iqshell/common/data"
+	"github.com/qiniu/qshell/v2/iqshell/common/log"
 
 	"github.com/spf13/cobra"
 )
@@ -13,47 +17,78 @@ var userCmd = &cobra.Command{
 	Short: "Manage users",
 }
 
-var userChCmd = &cobra.Command{
-	Use:   "cu [<AccountName>]",
-	Short: "Change user to AccountName",
-	Args:  cobra.RangeArgs(0, 1),
-	Run:   ChUser,
-}
-
+var lsCmdEg = `qshell user ls`
 var userLsCmd = &cobra.Command{
-	Use:   "ls",
-	Short: "List all accounts registered",
-	Run:   ListUser,
+	Use:     "ls",
+	Short:   "List all user registered",
+	Example: lsCmdEg,
+	Run:     ListUser,
 }
 
-var userCleanCmd = &cobra.Command{
-	Use:   "clean",
-	Short: "clean account db",
-	Long:  "Remove all users from inner dbs.",
-	Run:   CleanUser,
+var currentCmdEg = `qshell user current`
+var userCurrentCmd = &cobra.Command{
+	Use:     "current",
+	Short:   "get current user info",
+	Example: currentCmdEg,
+	Run:     CurrentUser,
 }
 
-var userRmCmd = &cobra.Command{
-	Use:   "remove <UserName>",
-	Short: "Remove user UID from inner db",
-	Args:  cobra.ExactArgs(1),
-	Run:   RmUser,
-}
-
+var lookupCmdEg = `qshell user lookup <UserName>`
 var userLookupCmd = &cobra.Command{
-	Use:   "lookup <UserName>",
-	Short: "Lookup user info by user name",
-	Args:  cobra.ExactArgs(1),
-	Run:   LookUp,
+	Use:     "lookup <UserName>",
+	Short:   "Lookup user info by user name",
+	Example: lookupCmdEg,
+	Run:     LookUp,
+}
+
+var addCmdEg = ` qshell user add <AK> <SK> <UserName>
+ or
+ qshell user add --ak <AK> --sk <SK> --name <UserName>`
+var userAddCmd = &cobra.Command{
+	Use:     "add",
+	Short:   "add user info to local",
+	Example: addCmdEg,
+	Run:     Add,
+}
+
+var changeUserCmdEg = `qshell user cu <UserName>`
+var userChCmd = &cobra.Command{
+	Use:     "cu [<UserName>]",
+	Short:   "Change user to UserName",
+	Example: changeUserCmdEg,
+	Run:     ChUser,
+}
+
+var cleanCmdEg = `qshell user clean`
+var userCleanCmd = &cobra.Command{
+	Use:     "clean",
+	Short:   "clean account db",
+	Long:    "Remove all users from inner dbs.",
+	Example: cleanCmdEg,
+	Run:     CleanUser,
+}
+
+var rmCmdEg = `qshell user remove <UserName>`
+var userRmCmd = &cobra.Command{
+	Use:     "remove <UserName>",
+	Short:   "Remove user info from inner db",
+	Example: rmCmdEg,
+	Run:     RmUser,
 }
 
 var (
 	userLsName bool
+	userAK     string
+	userSK     string
+	userName   string
 )
 
 func init() {
 	userLsCmd.Flags().BoolVarP(&userLsName, "name", "n", false, "only list user names")
-	userCmd.AddCommand(userChCmd, userLsCmd, userCleanCmd, userRmCmd, userLookupCmd)
+	userAddCmd.Flags().StringVarP(&userAK, "ak", "", "", "user's access key of Qiniu")
+	userAddCmd.Flags().StringVarP(&userSK, "sk", "", "", "user's secret key of Qiniu")
+	userAddCmd.Flags().StringVarP(&userName, "name", "", "", "user id of local")
+	userCmd.AddCommand(userAddCmd, userRmCmd, userCleanCmd, userChCmd, userLsCmd, userLookupCmd, userCurrentCmd)
 	RootCmd.AddCommand(userCmd)
 }
 
@@ -84,6 +119,17 @@ func ListUser(cmd *cobra.Command, params []string) {
 	}
 }
 
+// 获取当前账户信息
+// qshell user current
+func CurrentUser(cmd *cobra.Command, params []string) {
+	account, gErr := account.GetAccount()
+	if gErr != nil {
+		fmt.Println(gErr)
+		os.Exit(data.STATUS_ERROR)
+	}
+	fmt.Println(account.String())
+}
+
 // 删除本地记录的数据库
 // qshell user clean
 func CleanUser(cmd *cobra.Command, params []string) {
@@ -97,6 +143,10 @@ func CleanUser(cmd *cobra.Command, params []string) {
 // 删除用户
 // qshell user remove <UserName>
 func RmUser(cmd *cobra.Command, params []string) {
+	if len(params) == 0 {
+		log.Error(alert.CannotEmpty("user name", rmCmdEg))
+		return
+	}
 	userName := params[0]
 	err := account.RmUser(userName)
 	if err != nil {
@@ -108,10 +158,49 @@ func RmUser(cmd *cobra.Command, params []string) {
 // 查询用用户是否存在本地数据库中
 // qshell user lookup <UserName>
 func LookUp(cmd *cobra.Command, params []string) {
+	if len(params) == 0 {
+		log.Error(alert.CannotEmpty("user name", lookupCmdEg))
+		return
+	}
 	userName := params[0]
 	err := account.LookUp(userName)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "LookUp: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+// 查询用用户是否存在本地数据库中
+// qshell user add <AK> <SK> <UserName>
+func Add(cmd *cobra.Command, params []string) {
+	if len(params) == 3 {
+		userAK = params[0]
+		userSK = params[1]
+		userName = params[2]
+	}
+
+	// check
+	if len(userName) == 0 {
+		log.Error(alert.CannotEmpty("user name", addCmdEg))
+		return
+	}
+	if len(userAK) == 0 {
+		log.Error(alert.CannotEmpty("user ak", addCmdEg))
+		return
+	}
+	if len(userSK) == 0 {
+		log.Error(alert.CannotEmpty("user sk", addCmdEg))
+		return
+	}
+	sErr := account.SaveAccount(account.Account{
+		Name:      userAK,
+		AccessKey: userSK,
+		SecretKey: userName,
+	}, accountOver)
+
+	if sErr != nil {
+		fmt.Println(sErr)
+		os.Exit(data.STATUS_ERROR)
+	}
+
 }
---
+++ iqshell/common/alert/alert.go
@@ -0,0 +1,17 @@
+package alert
+
+func CannotEmpty(words string, suggest string) string {
+	desc := words
+	if len(words) > 0 {
+		desc += desc + " can't empty"
+	}
+	return Description(desc, suggest)
+}
+
+func Description(desc string, suggest string) string {
+	ret := desc
+	if len(suggest) > 0 {
+		ret += ", you can do like this:\n" + suggest
+	}
+	return ret
+}
