b27c9305b0513c5535dbbee367f6a57af80a6441
yangsen <yang_sen_ysys@163.com>
2021-12-28 16:11:39 +0800

    adjust sync

--- cmd/qupload.go
+++ cmd/qupload.go
@@ -74,10 +74,29 @@ var upload2CmdBuilder = func() *cobra.Command {
 	return cmd
 }
 
+var syncCmdBuilder = func() *cobra.Command {
+	info := operations.SyncUploadInfo{}
+	cmd := &cobra.Command{
+		Use:   "sync <SrcResUrl> <Buckets> [-k <Key>]",
+		Short: "Sync big file to qiniu bucket",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) > 0 {
+				info.ResourceUrl = args[0]
+				info.Bucket = args[1]
+			}
+		},
+	}
+	cmd.Flags().BoolVarP(&info.IsResumeV2, "resumable-api-v2", "", false, "use resumable upload v2 APIs to upload")
+	cmd.Flags().StringVarP(&info.UpHostIp, "uphost", "u", "", "upload host")
+	cmd.Flags().StringVarP(&info.Key, "key", "k", "", "save as <key> in bucket")
+	return cmd
+}
 
 func init() {
 	RootCmd.AddCommand(
 		uploadCmdBuilder(),
 		upload2CmdBuilder(),
+		syncCmdBuilder(),
 		)
 }
