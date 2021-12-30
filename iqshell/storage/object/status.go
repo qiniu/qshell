9217e09091ddff05035f190e0971f5d453703d2b
yangsen <yang_sen_ysys@163.com>
2021-12-30 19:11:37 +0800

    remove status_change.go

--- iqshell/storage/object/status.go
+++ iqshell/storage/object/status.go
@@ -2,6 +2,7 @@ package object
 
 import (
 	"errors"
+	"fmt"
 	"github.com/qiniu/go-sdk/v7/storage"
 	"github.com/qiniu/qshell/v2/iqshell/common/alert"
 	"github.com/qiniu/qshell/v2/iqshell/storage/object/batch"
@@ -20,5 +21,23 @@ func (s StatusApiInfo) ToOperation() (string, error) {
 }
 
 func Status(info StatusApiInfo) (batch.OperationResult, error) {
+	return batch.One(info)
+}
+
+// ChangeStatusApiInfo 修改 status
+type ChangeStatusApiInfo struct {
+	Bucket string
+	Key    string
+	Status int
+}
+
+func (c ChangeStatusApiInfo) ToOperation() (string, error) {
+	if len(c.Bucket) == 0 || len(c.Key) == 0 {
+		return "", errors.New(alert.CannotEmpty("change status operation bucket or key", ""))
+	}
+	return fmt.Sprintf("/chstatus/%s/status/%c", storage.EncodedEntry(c.Bucket, c.Key), c.Status), nil
+}
+
+func ChangeStatus(info ChangeStatusApiInfo) (batch.OperationResult, error) {
 	return batch.One(info)
 }
