7af401ef8fcf7e8a99f6e4ebbcab8822ce9778d8
yangsen <yang_sen_ysys@163.com>
2022-01-11 18:55:22 +0800

    add db

---
+++ iqshell/common/db/db.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+)
+
+type DB struct {
+	filePath string
+	db       *leveldb.DB
+}
+
+func OpenDB(filePath string) (*DB, error) {
+	db, err := leveldb.OpenFile(filePath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{
+		filePath: filePath,
+		db:       db,
+	}, nil
+}
+
+func (db *DB) Get(key string) (string, error) {
+	ret, err := db.db.Get([]byte(key), nil)
+	if err != nil {
+		return "", err
+	}
+	return string(ret), nil
+}
+
+func (db *DB) Put(key, value string) error {
+	return db.db.Put([]byte(key), []byte(value), &opt.WriteOptions{
+		Sync: true,
+	})
+}
