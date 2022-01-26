af0d6bffe12c2b64330f0b64fb4cad01206146a2
yangsen <yang_sen_ysys@163.com>
2022-01-26 12:06:32 +0800

    add fput and rput test case

--- cmd_test/upload_test.go
+++ cmd_test/upload_test.go
@@ -6,13 +6,30 @@ import (
 	"testing"
 )
 
-func TestFromUpload(t *testing.T) {
+func TestFormUpload(t *testing.T) {
+	path, err := test.CreateTempFile(1 * 1024)
+	if err != nil {
+		t.Fatal("create cdn config file error:", err)
+	}
+
+	result, errs := test.RunCmdWithError("fput", test.Bucket, "qshell_fput_1M", path)
+	if len(errs) > 0 {
+		t.Fail()
+	}
+
+	result = strings.ReplaceAll(result, "\n", "")
+	if !strings.Contains(result, "Upload File success") {
+		t.Fatal(result)
+	}
+}
+
+func TestResumeUpload(t *testing.T) {
 	path, err := test.CreateTempFile(5 * 1024)
 	if err != nil {
 		t.Fatal("create cdn config file error:", err)
 	}
 
-	result, errs := test.RunCmdWithError("rput",test.Bucket, "qshell_rput_5M", path)
+	result, errs := test.RunCmdWithError("rput", test.Bucket, "qshell_rput_5M", path)
 	if len(errs) > 0 {
 		t.Fail()
 	}
