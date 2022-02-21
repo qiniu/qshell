ddbe87e2100c1b322eab42ca2e024e58580fa23c
yangsen <yang_sen_ysys@163.com>
2022-02-21 16:59:55 +0800

    chtype & batchchtype add test case

--- cmd_test/change_type_test.go
+++ cmd_test/change_type_test.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"github.com/qiniu/qshell/v2/cmd_test/test"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -17,20 +19,83 @@ func TestChangeType(t *testing.T) {
 	}
 }
 
+func TestChangeTypeNoExistBucket(t *testing.T) {
+	_, errs := test.RunCmdWithError("chtype", test.BucketNotExist, test.Key, "0")
+	if !strings.Contains(errs, "no such bucket") {
+		t.Fail()
+	}
+}
+
+func TestChangeTypeNoExistKey(t *testing.T) {
+	_, errs := test.RunCmdWithError("chtype", test.Bucket, test.KeyNotExist, "0")
+	if !strings.Contains(errs, "no such file or directory") {
+		t.Fail()
+	}
+}
+
+func TestChangeTypeNoBucket(t *testing.T) {
+	_, errs := test.RunCmdWithError("chtype")
+	if !strings.Contains(errs, "Bucket can't empty") {
+		t.Fail()
+	}
+}
+
+func TestChangeTypeNoKey(t *testing.T) {
+	_, errs := test.RunCmdWithError("chtype", test.Bucket)
+	if !strings.Contains(errs, "Key can't empty") {
+		t.Fail()
+	}
+}
+
+func TestChangeTypeNoMimeType(t *testing.T) {
+	_, errs := test.RunCmdWithError("chtype", test.Bucket, test.Key)
+	if !strings.Contains(errs, "Type can't empty") {
+		t.Fail()
+	}
+}
+
+func TestChangeTypeDocument(t *testing.T) {
+	test.TestDocument("chtype", t)
+}
+
 func TestBatchChangeType(t *testing.T) {
 	batchConfig := ""
-	for _, key := range test.Keys {
+	keys := test.Keys
+	keys = append(keys, "hello10.json")
+	for _, key := range keys {
 		batchConfig += key + "\t" + "0" + "\n"
 	}
+
+	resultDir, err := test.ResultPath()
+	if err != nil {
+		t.Fatal("get result dir error:", err)
+	}
+
+	successLogPath := filepath.Join(resultDir, "batch_chtype_success.txt")
+	failLogPath :=  filepath.Join(resultDir, "batch_chtype_fail.txt")
 
 	path, err := test.CreateFileWithContent("batch_chtype.txt", batchConfig)
 	if err != nil {
 		t.Fatal("create cdn config file error:", err)
 	}
 
-	_, errs := test.RunCmdWithError("batchchtype", test.Bucket, "-i", path, "-y")
-	if len(errs) > 0 {
-		t.Fail()
+	test.RunCmdWithError("batchchtype", test.Bucket,
+		"-i", path,
+		"--success-list", successLogPath,
+		"--failure-list", failLogPath,
+		"--worker", "4",
+		"-y")
+	defer func() {
+		test.RemoveFile(successLogPath)
+		test.RemoveFile(failLogPath)
+	}()
+
+	if !test.IsFileHasContent(successLogPath) {
+		t.Fatal("batch result: success log to file error: file empty")
+	}
+
+	if !test.IsFileHasContent(failLogPath) {
+		t.Fatal("batch result: fail log  to file error: file empty")
 	}
 
 	//back
@@ -44,8 +109,12 @@ func TestBatchChangeType(t *testing.T) {
 		t.Fatal("create cdn config file error:", err)
 	}
 
-	_, errs = test.RunCmdWithError("batchchtype", test.Bucket, "-i", path, "-y")
+	_, errs := test.RunCmdWithError("batchchtype", test.Bucket, "-i", path, "-y")
 	if len(errs) > 0 {
 		t.Fail()
 	}
+}
+
+func TestBatchChangeTypeDocument(t *testing.T) {
+	test.TestDocument("batchchtype", t)
 }
