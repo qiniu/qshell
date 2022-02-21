733f56c951bc47e24db44b982806809d6ee60f32
yangsen <yang_sen_ysys@163.com>
2022-02-21 17:35:42 +0800

    delete & batchdelete add test case

--- cmd_test/delete_test.go
+++ cmd_test/delete_test.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"github.com/qiniu/qshell/v2/cmd_test/test"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -18,21 +20,83 @@ func TestDelete(t *testing.T) {
 	}
 }
 
+func TestDeleteNoExistBucket(t *testing.T) {
+	_, errs := test.RunCmdWithError("delete", test.BucketNotExist, test.Key)
+	if !strings.Contains(errs, "no such bucket") {
+		t.Fail()
+	}
+}
+
+func TestDeleteNoExistKey(t *testing.T) {
+	_, errs := test.RunCmdWithError("delete", test.Bucket, test.KeyNotExist)
+	if !strings.Contains(errs, "no such file or directory") {
+		t.Fail()
+	}
+}
+
+func TestDeleteNoBucket(t *testing.T) {
+	_, errs := test.RunCmdWithError("delete")
+	if !strings.Contains(errs, "Bucket can't empty") {
+		t.Fail()
+	}
+}
+
+func TestDeleteNoKey(t *testing.T) {
+	_, errs := test.RunCmdWithError("delete", test.Bucket)
+	if !strings.Contains(errs, "Key can't empty") {
+		t.Fail()
+	}
+}
+
+func TestDeleteDocument(t *testing.T) {
+	test.TestDocument("delete", t)
+}
+
 func TestBatchDelete(t *testing.T) {
+	TestBatchCopy(t)
+
 	batchConfig := ""
-	for _, key := range test.Keys {
+	keys := test.Keys
+	keys = append(keys, "hello10.json")
+	for _, key := range keys {
 		batchConfig += "copy_" + key + "\n"
 	}
+
+	resultDir, err := test.ResultPath()
+	if err != nil {
+		t.Fatal("get result dir error:", err)
+	}
+
+	successLogPath := filepath.Join(resultDir, "batch_delete_success.txt")
+	failLogPath :=  filepath.Join(resultDir, "batch_delete_fail.txt")
 
 	path, err := test.CreateFileWithContent("batch_delete.txt", batchConfig)
 	if err != nil {
 		t.Fatal("create cdn config file error:", err)
 	}
 
-	_, errs := test.RunCmdWithError("batchdelete", test.Bucket, "-i", path, "-y")
-	if len(errs) > 0 {
-		t.Fail()
+	test.RunCmdWithError("batchdelete", test.Bucket,
+		"-i", path,
+		"--success-list", successLogPath,
+		"--failure-list", failLogPath,
+		"--worker", "4",
+		"-y")
+	defer func() {
+		test.RemoveFile(successLogPath)
+		test.RemoveFile(failLogPath)
+	}()
+
+	if !test.IsFileHasContent(successLogPath) {
+		t.Fatal("batch result: success log to file error: file empty")
 	}
+
+	if !test.IsFileHasContent(failLogPath) {
+		t.Fatal("batch result: fail log  to file error: file empty")
+	}
+}
+
+func TestBatchDeleteDocument(t *testing.T) {
+	test.TestDocument("batchdelete", t)
 }
 
 func TestDeleteAfter(t *testing.T) {
