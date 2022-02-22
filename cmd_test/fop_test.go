0649dd8cfb6613a3c4d25f1a242313be6814400b
yangsen <yang_sen_ysys@163.com>
2022-02-22 17:41:44 +0800

    pfop add test case

--- cmd_test/fop_test.go
+++ cmd_test/fop_test.go
@@ -6,8 +6,13 @@ import (
 	"testing"
 )
 
+const fopObjectKey = "test_mv.mp4"
+const fopObjectValue = "avthumb/mp4"
 func TestFop(t *testing.T) {
-	result, errs := test.RunCmdWithError("pfop", test.Bucket, "test_mv.mp4", "avthumb/mp4")
+	result, errs := test.RunCmdWithError("pfop", test.Bucket, fopObjectKey, fopObjectValue,
+		"--notify-url", "",
+		"--pipeline", "true",
+		"--force", "true")
 	if len(errs) > 0 {
 		t.Fail()
 	}
@@ -15,6 +20,41 @@ func TestFop(t *testing.T) {
 	result = strings.ReplaceAll(result, "\n", "")
 	result, errs = test.RunCmdWithError("prefop", result)
 	if len(errs) > 0 {
+		t.Fail()
+	}
+}
+
+func TestFopNoExistBucket(t *testing.T) {
+	_, errs := test.RunCmdWithError("pfop", test.BucketNotExist, fopObjectKey, fopObjectValue)
+	if !strings.Contains(errs, "no such bucket") {
+		t.Fail()
+	}
+}
+
+func TestFopNoExistKey(t *testing.T) {
+	_, errs := test.RunCmdWithError("pfop", test.Bucket, test.KeyNotExist, fopObjectValue)
+	if !strings.Contains(errs, "invalid_param") {
+		t.Fail()
+	}
+}
+
+func TestFopNoBucket(t *testing.T) {
+	_, errs := test.RunCmdWithError("pfop")
+	if !strings.Contains(errs, "Bucket can't empty") {
+		t.Fail()
+	}
+}
+
+func TestFopNoKey(t *testing.T) {
+	_, errs := test.RunCmdWithError("pfop", test.Bucket)
+	if !strings.Contains(errs, "Key can't empty") {
+		t.Fail()
+	}
+}
+
+func TestFopNoFopValue(t *testing.T) {
+	_, errs := test.RunCmdWithError("pfop", test.Bucket, fopObjectKey)
+	if !strings.Contains(errs, "Fops can't empty") {
 		t.Fail()
 	}
 }
