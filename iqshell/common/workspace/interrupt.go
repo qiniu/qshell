5c6859d1c13d996660a0a4b62b88c35a4b57613c
yangsen <yang_sen_ysys@163.com>
2022-03-09 19:09:33 +0800

    isCmdInterrupt add lock

--- iqshell/common/workspace/interrupt.go
+++ iqshell/common/workspace/interrupt.go
@@ -4,18 +4,19 @@ import (
 	"github.com/qiniu/qshell/v2/iqshell/common/data"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"time"
 
 	"github.com/qiniu/qshell/v2/iqshell/common/log"
 )
 
 var (
-	// 程序是否终端
-	isCmdInterrupt = false
+	// 程序是否退出
+	isCmdInterrupt uint32 = 0
 )
 
 func IsCmdInterrupt() bool {
-	return isCmdInterrupt
+	return atomic.LoadUint32(&isCmdInterrupt) > 0
 }
 
 func observerCmdInterrupt() {
@@ -25,7 +26,7 @@ func observerCmdInterrupt() {
 		si := <-s
 		log.Alert("")
 		log.DebugF("Got signal:%s", si)
-		isCmdInterrupt = true
+		atomic.StoreUint32(&isCmdInterrupt, 1)
 		Cancel()
 		time.Sleep(time.Millisecond * 500)
 		os.Exit(data.StatusUserCancel)
