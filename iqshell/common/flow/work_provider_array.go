ecc01a67e730e690c4a760940efe9a29b045b2f2
YangSen-qn <yangsen@qiniu.com>
2022-03-31 16:21:11 +0800

    add array work provide

--- iqshell/common/flow/work_provider_array.go
+++ iqshell/common/flow/work_provider_array.go
@@ -1,1 +1,33 @@
 package flow
+
+import "sync"
+
+func NewArrayWorkProvider(works []Work) WorkProvider {
+	return &arrayWorkProvider{
+		readOffset: 0,
+		works:      works,
+	}
+}
+
+type arrayWorkProvider struct {
+	mu         sync.Mutex
+	readOffset int
+	works      []Work
+}
+
+func (a *arrayWorkProvider) Provide() (hasMore bool, work Work, err error) {
+	a.mu.Lock()
+	hasMore, work, err = a.provide()
+	a.mu.Unlock()
+	return
+}
+
+func (a *arrayWorkProvider) provide() (hasMore bool, work Work, err error) {
+	if a.readOffset > len(a.works)-1 {
+		return false, nil, nil
+	}
+	hasMore = true
+	work = a.works[a.readOffset]
+	a.readOffset ++
+	return
+}
