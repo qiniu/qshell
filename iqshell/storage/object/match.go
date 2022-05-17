14ca25e35124e9768321997ed54c9d5582444bc7
YangSen-qn <yangsen@qiniu.com>
2022-05-17 22:22:33 +0800

    optimize match output

--- iqshell/storage/object/match.go
+++ iqshell/storage/object/match.go
@@ -10,10 +10,10 @@ import (
 )
 
 type MatchApiInfo struct {
-	Bucket    string
-	Key       string
-	FileHash  string
-	LocalFile string
+	Bucket    string // 文件所在七牛云的空间名，必选
+	Key       string // 文件所在七牛云的 Key， 必选
+	FileHash  string // 文件 Etag，可以是 etagV1, 也可以是 etagV2；可选，没有会从服务获取
+	LocalFile string // 本地文件路径；必选
 }
 
 func (m *MatchApiInfo) WorkId() string {
@@ -37,7 +37,7 @@ func Match(info MatchApiInfo) (match *MatchResult, err *data.CodeError) {
 
 	hashFile, oErr := os.Open(info.LocalFile)
 	if oErr != nil {
-		return nil, data.NewEmptyError().AppendDesc("Match Check: get local file error:" + oErr.Error())
+		return nil, data.NewEmptyError().AppendDescF("Match Check: get local file error:%v", oErr)
 	}
 
 	var serverObjectStat *StatusResult
@@ -47,7 +47,7 @@ func Match(info MatchApiInfo) (match *MatchResult, err *data.CodeError) {
 			Key:      info.Key,
 			NeedPart: true,
 		}); sErr != nil {
-			return nil, data.NewEmptyError().AppendDesc("Match Check: get file status error:" + sErr.Error())
+			return nil, data.NewEmptyError().AppendDescF("Match Check: get file status error:%v", sErr)
 		} else {
 			info.FileHash = stat.Hash
 			serverObjectStat = &stat
@@ -57,20 +57,20 @@ func Match(info MatchApiInfo) (match *MatchResult, err *data.CodeError) {
 	// 计算本地文件 hash
 	var hash string
 	if utils.IsSignByEtagV2(info.FileHash) {
-		log.Debug("Match Check: get etag by v2 for key:%s", info.Key)
+		log.DebugF("Match Check: get etag by v2 for key:%s", info.Key)
 		if serverObjectStat == nil {
 			if stat, sErr := Status(StatusApiInfo{
 				Bucket:   info.Bucket,
 				Key:      info.Key,
 				NeedPart: true,
 			}); sErr != nil {
-				return nil, data.NewEmptyError().AppendDesc("Match Check: etag v2, get file status error:" + sErr.Error())
+				return nil, data.NewEmptyError().AppendDescF("Match Check: etag v2, get file status error:%v", sErr)
 			} else {
 				serverObjectStat = &stat
 			}
 		}
 		if h, eErr := utils.EtagV2(hashFile, serverObjectStat.Parts); eErr != nil {
-			return nil, data.NewEmptyError().AppendDesc("Match Check: get file etag v2 error:" + eErr.Error())
+			return nil, data.NewEmptyError().AppendDescF("Match Check: get file etag v2 error:%v", eErr)
 		} else {
 			hash = h
 		}
@@ -78,7 +78,7 @@ func Match(info MatchApiInfo) (match *MatchResult, err *data.CodeError) {
 	} else {
 		log.DebugF("Match Check: get etag by v1 for key:%s", info.Key)
 		if h, eErr := utils.EtagV1(hashFile); eErr != nil {
-			return nil, data.NewEmptyError().AppendDesc("Match Check: get file etag v1 error:" + eErr.Error())
+			return nil, data.NewEmptyError().AppendDescF("Match Check: get file etag v1 error:%v", eErr)
 		} else {
 			hash = h
 		}
@@ -86,7 +86,7 @@ func Match(info MatchApiInfo) (match *MatchResult, err *data.CodeError) {
 	}
 	log.DebugF("Match Check:       server hash, key:%s hash:%s", info.Key, hash)
 	if hash != info.FileHash {
-		return nil, data.NewEmptyError().AppendDesc("Match Check: file hash doesn't match for key:" + info.Key + "download file hash:" + hash + " excepted:" + info.FileHash)
+		return nil, data.NewEmptyError().AppendDescF("Match Check: file hash doesn't match for key:%s, local file hash:%s server file hash:%s", info.Key, hash, info.FileHash)
 	}
 
 	return &MatchResult{
