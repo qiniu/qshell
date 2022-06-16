b9329a7256e15ccb5e4f0a60e361f96207291c8f
yangsen <yang_sen_ysys@163.com>
2022-06-16 16:14:36 +0800

    optimize batchforbbden doc

--- docs/batchforbidden.go
+++ docs/batchforbidden.go
@@ -5,7 +5,7 @@ import _ "embed"
 //go:embed batchforbidden.md
 var batchForbiddenDocument string
 
-const BatchForbiddenType = "batchfetch"
+const BatchForbiddenType = "batchforbidden"
 
 func init() {
 	addCmdDocumentInfo(BatchForbiddenType, batchForbiddenDocument)
