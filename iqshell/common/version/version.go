117af784dc1428e7c860ccb6edcea1b4c615c1b1
jimyag <jiaminyag@gmail.com>
2024-02-03 00:41:50 +0800

    feat: support go install

--- iqshell/common/version/version.go
+++ iqshell/common/version/version.go
@@ -1,7 +1,36 @@
 package version
 
-var version = "UNSTABLE"
+import (
+	"runtime/debug"
+)
+
+var (
+	defaultVersion = "UNSTABLE"
+	version        = defaultVersion
+)
 
 func Version() string {
-	return version
+	// 通过 LDFLAGS 设置的版本号
+	if version != defaultVersion {
+		return version
+	}
+
+	v, ok := buildInfoVersion()
+	if !ok {
+		return version
+	}
+
+	
+	if v == "(devel)" {
+		return version
+	}
+	return v
+}
+
+func buildInfoVersion() (string, bool) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "", false
+	}
+	return info.Main.Version, true
 }
